09755a7afec95be378a5ea05bf32e1a145bc0cb6
Kapil Bagul <kapilbagul@Kapils-MacBook-Pro.local>
2020-02-12 23:42:27 -0500

    Router

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
@@ -11,6 +12,10 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
 
 
 
@@ -19,7 +24,10 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
@@ -27,6 +35,7 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       FormsModule,
       BsDropdownModule.forRoot(),
       BrowserAnimationsModule,
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
